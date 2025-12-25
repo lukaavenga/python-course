8a6f9efd44b03ce27546f491925372c0fa7d5426
Luka Telen <luka.telen@avenga.com>
2025-12-25 10:53:13 +0100

    Fix formatting

--- module_basics/practice_1/main.py
+++ module_basics/practice_1/main.py
@@ -3,50 +3,22 @@ def main():
 
     a = input("Enter first number: ")
     b = input("Enter second number: ")
+
     sum_ab = float(a) + float(b)
-    substract_ab = float(a) - float(b)
+    subtract_ab = float(a) - float(b)
     multiply_ab = float(a) * float(b)
     divide_ab = float(a) / float(b)
     floor_divide_ab = float(a) // float(b)
     modulus_ab = float(a) % float(b)
     exponent_ab = float(a) ** float(b)
-    print("The difference of", a, "and", b, "is:", substract_ab)
+
+    print("The difference of", a, "and", b, "is:", subtract_ab)
     print("The product of", a, "and", b, "is:", multiply_ab)
     print("The division of", a, "by", b, "is:", divide_ab)
     print("The floor division of", a, "by", b, "is:", floor_divide_ab)
     print("The modulus of", a, "and", b, "is:", modulus_ab)
     print("The", a, "raised to the power of", b, "is:", exponent_ab)
     print("The sum of", a, "and", b, "is:", sum_ab)
-    
-if __name__ == "__main__":
-    main()
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
